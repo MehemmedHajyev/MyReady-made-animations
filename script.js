04ac5e5de0e465773c1fff1624d5f46054c2babd
MehemmedHajyev <hajyeffmehemmed8@gmail.com>
2024-01-02 15:37:14 +0400

    first-commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let animationContainer = document.querySelector(".animation-container");
+
+let anımatıonData = [
+  {
+    id: 1,
+    name: "Mehemmed",
+    img:'test',
+    imgAlt:'noteFound'
+  },
+  {
+    id: 2,
+    name: "Mehemmed2",
+  },
+
+  {
+    id: 2,
+    name: "Mehemmed2",
+  },  {
+    id: 2,
+    name: "Mehemmed2",
+  },
+];
+
+anımatıonData.map((item, key) => {
+  animationContainer.innerHTML += `
+  <div class="animation-card">
+            <img src="" alt=${item.imgAlt}>
+            <div class="animation-card-hover">
+                <p>${item.name}</p>
+                <span>${item.id}</span>
+            </div>
+        </div>
+
+`;
+});
